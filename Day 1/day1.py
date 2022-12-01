96e1859a0ac0ee5b01a76434fb0002d64c0a085b
Joe Moore <98429175+Frqcture@users.noreply.github.com>
2022-12-01 12:31:33 +0000

    Update day1.py

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,29 +1,24 @@
+def day1():
+    f = open("/Users/joemoore/Documents/AdventOfCode/AdventOfCode2022/Day 1/data.txt", "r")
 
-f = open("data.txt", "r")
+    data = f.readlines()
+    elves = [0]
+    elfcounter = 0
 
-data = f.readlines()
-elves = [0]
-elfcounter = 0
-for i in range(len(data) - 1):
-    if data[i] == "\n":
-        elfcounter = elfcounter + 1
-        elves.append(0)
-    else:
-        thing = data[i].strip()
-        elves[elfcounter] = elves[elfcounter] + int(thing)
+    for i in range(len(data) - 1):
+        if data[i] == "\n":
+            elfcounter = elfcounter + 1
+            elves.append(0)
+        else:
+            thing = data[i].strip()
+            elves[elfcounter] = elves[elfcounter] + int(thing)
 
-biggest = 0
-index = 0
+    elves.sort()
 
-for j in range(len(elves) - 1):
-    if elves[j] > biggest:
-        biggest = elves[j]
-        index = j
+    print(elves[len(elves) - 1])
 
-print(str(biggest) + " " + str(j))
+    print(elves[len(elves) - 1] + elves[len(elves) - 2] + elves[len(elves) - 3])
 
-elves.sort()
+    f.close
 
-print(elves[len(elves) - 1] + elves[len(elves) - 2] + elves[len(elves) - 3])
-
-f.close
+day1()
